d4917f1ff59225bca279f1eb1bed977da67794e6
Fiona Ross <campbell796@gmail.com>
2021-12-19 14:20:50 -0500

    still finishing 1.5 excercise 4B...moving at a snails pace...

--- Level1/Section 1.5/Excercise 4/Level1_Section1.5_Exercise4/Level1_Section1.5_Exercise4.cpp
+++ Level1/Section 1.5/Excercise 4/Level1_Section1.5_Exercise4/Level1_Section1.5_Exercise4.cpp
@@ -30,10 +30,20 @@ template <typename T, typename Vec, typename Return_t>
 struct useAdapters
 {
     // Statistical Properties 
-    static Return_t numAnalysis(const Vec& inVec, T tgtVal, std::size_t N)
+    static Return_t tgtIndex(const Vec& inVec, T tgtVal, std::size_t N)
     {
         // Part A) sorted numeric vector v and a target value x , 
         //         find the first index i, such that: v[i] <= x < v[i + 1]?
+
+        // Edge Cases: 
+        // 1. The target value is not in the vector range (ie. x = 0 or 4 & v = {1,2,3})
+        //         ** if x = 0 then will return "not in range" and output -1 
+        //         ** if x = 4 then will return "not in range" and output inVecTmp.end()
+        // 2. The target value is the last value in the vector range so no i+1 or upper bound
+        //          (ie. x = 4 & v = {1,2,3,4) or v = {1,2,3,4,4})
+        //          (ie. x = 4.3 & v = {1,2,3,4} 
+        //          ** will return lower bound but "upper bound not in range" and upper bound is inVecTmp.end()
+
 
         // Change N to an int M so can use for indexing 
         int M = static_cast<int>(N);
@@ -42,7 +52,7 @@ struct useAdapters
         std::sort(inVecTmp.begin(), inVecTmp.end());
 
         // Printing contents of vector
-        std::cout << "Vector is: "; 
+        std::cout << "\nVector is: "; 
         for (size_t i = 0; i < inVecTmp.size(); ++i) {std::cout << inVecTmp.at(i) << "; "; }
 
         // first check if the target value (x) exists in the vector
@@ -50,70 +60,130 @@ struct useAdapters
             // since target exists can work to finding i-1 and i+1 
         {
             // checking to see if there are multiple target values 
-            std::cout << "entered if loop" << std::endl;
+            std::cout << "\nEntered if loop";
             auto lBnd = std::find(inVecTmp.begin(), inVecTmp.end(), tgtVal);
             auto nxtVal = std::find(inVecTmp.begin(), inVecTmp.end(), tgtVal);
-           
+
+            std::cout << "\nTarget is: " << tgtVal;
+            std::cout << "\nLower bound is: " << lBnd - inVecTmp.begin();
+
             if (nxtVal != inVecTmp.end())
             {
                 // setting upper bound the same as lower bound and then will increment
-                auto uBnd = std::find(inVecTmp.begin(), inVecTmp.end(), tgtVal); 
+                auto uBnd = std::find(inVecTmp.begin(), inVecTmp.end(), tgtVal);
+                
                 // Part B) if multiple target values use distance to find how many  
                 auto uTgt = std::upper_bound(lBnd, inVecTmp.end(), tgtVal);
+                
                 // finding the number of tgt values using distance (returns an int) 
                 int dist = std::distance(lBnd, uTgt);
+                
                 // increasing lBnd by the number of tgt values to get upper bound  
-                std::advance(uBnd, dist); 
+                std::advance(uBnd, dist);
+                std::cout << "\nUpper bound is: " << uBnd - inVecTmp.begin();
             }
-            return 0.0;
-        }
+            else
+            {
+                std::cout << "\nUpper bound is: " << nxtVal - inVecTmp.begin()<< std::endl;
+            }
+
+            // Getting return values: 
+            int intIt = inVecTmp[0] + std::distance(inVecTmp.begin(), std::find(inVecTmp.begin(), inVecTmp.end(), tgtVal));
+
+            return intIt;
+        } // end of if (std::find(inVecTmp.begin(), inVecTmp.end(), tgtVal) != inVecTmp.end())
 
         else if ((inVecTmp[0] <= tgtVal) && (inVecTmp[M - 1] > tgtVal)) // if target does not exist then need to see if target value is btwn vector values 
         {
-            std::cout << "entered else loop" << std::endl; 
-           
+            std::cout << "\nEntered else loop";
+
 
             // find what indexes it is inbetween 
-            for (int i = 1; i < M ; i++) //iterator through finding min distance value 
+            for (int i = 1; i < M; i++) //iterator through finding min distance value 
             {
-                std::cout << "\ni is: " << i;
-                std::cout << "\nM is: " << M;
-                
                 // checking to see if the target value is in range & if so find upper & lower bound 
-                if (tgtVal - inVecTmp[i-1] >= 0 && tgtVal - inVecTmp[i] <= 0)
+                if (tgtVal - inVecTmp[i - 1] >= 0 && tgtVal - inVecTmp[i] <= 0)
                 {
                     T Lbnd = inVecTmp[i - 1];
                     T Ubnd = inVecTmp[i];
-                    int Lidx = i-1;
+                    int Lidx = i - 1;
                     std::cout << "\nTarget is: " << tgtVal;
-                    std::cout << "\nLower bound is: " << Lbnd; 
-                    std::cout << "\nUpper bound is: " << Ubnd;
-                    std::cout << "\nIndex is: " << Lidx; 
+                    std::cout << "\nLower bound is: " << i - 1;
+                    std::cout << "\nUpper bound is: " << i; 
+                    std::cout << "\nLower value is: " << Lbnd;
+                    std::cout << "\nUpper value is: " << Ubnd << std::endl;
+                    
+                    // returning lower index 
+                    return Lidx;
                 }
                 // Do not need to check for multiple values
             }
-            return 0.0;       
+
+        } // end of ((inVecTmp[0] <= tgtVal) && (inVecTmp[M - 1] > tgtVal))
+
+        else
+        {
+            std::cout << "\nTarget value is not in range";
+            if (tgtVal < inVecTmp[0])
+            {
+                std::cout << "\nTarget value is less than smallest value";
+                // returning error value int error = -1 
+                int error = -1; 
+                return error;
+            }
+            else
+            {
+                std::cout << "\nTarget value is greater than largest value";
+                int error = M-1;
+                return error;
+            }
+
+        } // end of else 
+    } // end function --> numAnalysis(const Vec& inVec, T tgtVal, std::size_t N)
+
+
+    static std::vector<double> vectMax(const Vec& inVec1, const Vec& inVec2, std::size_t N, int i, int j)
+    {
+        // Change N to an int M so can use for indexing 
+        int M = static_cast<int>(N);
+
+        // first sort vector in case not sorted 
+        Vec inVecTmp1 = inVec1; // making local copy since const 
+        Vec inVecTmp2 = inVec2; // making local copy since const 
+        std::sort(inVecTmp1.begin(), inVecTmp1.end());
+        std::sort(inVecTmp2.begin(), inVecTmp2.end());
+
+        // Printing contents of two vectors 
+        std::cout << "\nVector 1 is: ";
+        for (size_t i = 0; i < inVecTmp1.size(); ++i) { std::cout << inVecTmp1.at(i) << "; "; }
+       
+        std::cout << "\nVector 2 is: ";
+        for (size_t i = 0; i < inVecTmp2.size(); ++i) { std::cout << inVecTmp2.at(i) << "; "; }
+
+        // first check that vectors are larger than second index value 
+        if (j >= inVecTmp1.end() || j >= inVecTmp2.end())
+        {
+            std::cout << "\nJ index is out of bounds of one of the vectors"; 
+            return -1; 
         }
 
+        else
+        {
+            double sum = 0; 
+            for (int iter = i; iter = j; iter++)
+            {
 
-        //
-       
-        //int* p; // pointer 
-        //p = std::find(inVecTmp.begin(), inVecTmp.end()-1, tgtVal);
-        //if (p != inVecTmp.end() - 1)
-        //    return inVecTmp.end(); 
-
-        //
-
-        
+                int normDim = std::distance()
+                sum += ((inVecTmp1[iter]-inVecTmp2[iter])^normDim)
+            }
+        }
     }
 };
 
 int main()
 {
-
     // creating use case where target value exists 
-    std::cout << "\nFour element vector tgt exists: \n";
+    std::cout << "\nFour element vector tgt exists:";
     const std::size_t M = 4;
     std::vector<double> b4(M);
     double val4 = 3;
@@ -122,11 +192,28 @@ int main()
     std::for_each(b4.begin(), b4.end(), [&val4, &incr, &j](double& k) {k = val4 + incr[j]; j++; });
     double tgtExst = 4; 
         
-    double b4Out1 = useAdapters<double, std::vector<double>, double>::numAnalysis(b4, tgtExst, M);
+    double b4Out1 = useAdapters<double, std::vector<double>, double>::tgtIndex(b4, tgtExst, M);
 
     // creating use case where target value is between elements in vector
-    std::cout << "\nFour element vector tgt is btwn elements: \n";
+    std::cout << "\nFour element vector tgt is btwn elements:";
     double tgtBtwn = 4.5;
-    double b4Out2 = useAdapters<double, std::vector<double>, double>::numAnalysis(b4, tgtBtwn, M);
+    double b4Out2 = useAdapters<double, std::vector<double>, double>::tgtIndex(b4, tgtBtwn, M);
+
+    // creating use case where target value is smaller than first element in vector
+    std::cout << "\nFour element vector tgt is smallest than first element:";
+    double tgtSmall = 0.1;
+    double b4Out3 = useAdapters<double, std::vector<double>, double>::tgtIndex(b4, tgtSmall, M);
+
+    // creating use case where target value is greater than first element in a vector
+    std::cout << "\nFour element vector tgt is larger than greatest element:";
+    double tgtLarge = 10.1;
+    double b4Out4 = useAdapters<double, std::vector<double>, double>::tgtIndex(b4, tgtLarge, M);
+
+    // Printing all the index values and types 
+    std::cout<<"\n";
+    std::cout << "\nb4Out1 is: " << b4Out1 << " Type is: " << typeid(b4Out1).name(); 
+    std::cout << "\nb4Out2 is: " << b4Out2 << " Type is: " << typeid(b4Out2).name();
+    std::cout << "\nb4Out3 is: " << b4Out3 << " Type is: " << typeid(b4Out3).name();
+    std::cout << "\nb4Out3 is: " << b4Out4 << " Type is: " << typeid(b4Out4).name();
 
 }
